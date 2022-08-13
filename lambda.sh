3f0eb9f593473d71272e59f7f7f6674c19701748
Hattomo (TomohiroHattori) <45988993+Hattomo@users.noreply.github.com>
2022-08-14 00:15:54 +0900

    Create lambda.sh

---
+++ lambda.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -e "useage : ./lambda.sh <function_name>\n"
+dir=$1
+path=./server/$dir
+cd $path
+export DEBIAN_FRONTEND=noninteractive
+echo -e "\nBuilding...🚀"
+GOOS=linux GOARCH=amd64 go build -o hello main.go
+echo -e "\nZipping...🛸"
+zip hello.zip hello
+echo -e "\nDeploying...🛰️"
+aws lambda update-function-code --function-name $dir --zip-file fileb://hello.zip
+echo -e "Cleaning...🎉"
+rm hello.zip && rm hello
