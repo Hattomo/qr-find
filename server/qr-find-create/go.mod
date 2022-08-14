cbb512c97012849a2ae08eb8433e19b5284802a5
Hattomo (TomohiroHattori) <45988993+Hattomo@users.noreply.github.com>
2022-08-14 14:00:54 +0900

    lambda dynamodb

--- server/qr-find-create/go.mod
+++ server/qr-find-create/go.mod
@@ -2,4 +2,9 @@ module qr-find-create
 
 go 1.19
 
-require github.com/aws/aws-lambda-go v1.34.1
+require (
+	github.com/aws/aws-lambda-go v1.34.1
+	github.com/aws/aws-sdk-go v1.44.76
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- server/qr-find-create/main.go
+++ server/qr-find-create/main.go
@@ -1,25 +1,91 @@
+// Reference
+// https://qiita.com/saki-engineering/items/e5cf56301d94ceea3ce0
+
 package main
 
 import (
-	"context"
-	"fmt"
+    "encoding/json"
 
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+    "github.com/aws/aws-lambda-go/events"
+    "github.com/aws/aws-lambda-go/lambda"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
-func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
-	fmt.Printf("Body size = %d.\n", len(request.Body))
+// Item DBに入れるデータ
+type Item struct {
+    ID  string    `dynamodbav:"id" json:id`
+    Email   string `dynamodbav:"email" json:email`
+    Memo  string `dynamodbav:"memo" json:memo`
+}
 
-	fmt.Println("Headers:")
-	for key, value := range request.Headers {
-		fmt.Printf("    %s: %s\n", key, value)
-	}
+// Response Lambdaが返答するデータ
+type Response struct {
+    RequestMethod string `json:RequestMethod`
+    Result        Item   `json:Result`
+}
 
-	return events.APIGatewayProxyResponse{Body: request.Body + "Apple\n", StatusCode: 200}, nil
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+    method := request.HTTPMethod
+
+    // DBと接続するセッションを作る→DB接続
+    sess, err := session.NewSession()
+    if err != nil {
+        return events.APIGatewayProxyResponse{
+            Body:       err.Error(),
+            StatusCode: 500,
+        }, err
+    }
+
+    db := dynamodb.New(sess)
+
+    // リクエストボディのjsonから、Item構造体(DB用データの構造体)を作成
+    reqBody := request.Body
+    resBodyJSONBytes := ([]byte)(reqBody)
+    item := Item{}
+    if err := json.Unmarshal(resBodyJSONBytes, &item); err != nil {
+        return events.APIGatewayProxyResponse{
+            Body:       err.Error(),
+            StatusCode: 500,
+        }, err
+    }
+
+    // Item構造体から、inputするデータを用意
+    inputAV, err := dynamodbattribute.MarshalMap(item)
+    if err != nil {
+        return events.APIGatewayProxyResponse{
+            Body:       err.Error(),
+            StatusCode: 500,
+        }, err
+    }
+    input := &dynamodb.PutItemInput{
+        TableName: aws.String("find_qr"),
+        Item:      inputAV,
+    }
+
+    // insert実行
+    _, err = db.PutItem(input)
+    if err != nil {
+        return events.APIGatewayProxyResponse{
+            Body:       err.Error(),
+            StatusCode: 500,
+        }, err
+    }
+
+    // httpレスポンス作成
+    res := Response{
+        RequestMethod: method,
+    }
+    jsonBytes, _ := json.Marshal(res)
+
+    return events.APIGatewayProxyResponse{
+        Body:       string(jsonBytes),
+        StatusCode: 200,
+    }, nil
 }
 
 func main() {
-	lambda.Start(handleRequest)
+    lambda.Start(handler)
 }
